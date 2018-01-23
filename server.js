8ed0f6b2e644b4ca89091948ac39d73f3557fe42
Fabien Lebas <fabien.lebas@decathlon.com>
2018-01-23 19:05:10 +0100

    Presque OK

--- server.js
+++ server.js
@@ -1,14 +1,31 @@
 const express = require("express");
 const app = express();
+const { Client } = require('pg');
+const connectionString = process.env.REACT_APP_DATABASE_URL;
 
-const port = process.env.PORT || 4000;
+console.log(connectionString);
+
+const port = process.env.PORT || 3000;
 
 app.listen(port, function () {
   console.log("Server listening on port:" + port);
 });
 
 app.get("/api", function (request, result) {
-  result.send("Products API");
+  const client = new Client({
+    connectionString: connectionString,
+    ssl: true
+  });
+  client.connect();
+  client.query('SELECT * FROM products', (err, res) => {
+    if(err){
+      console.warn("error during query : " + err);
+      result.send("Products API error");
+    } else {
+      result.json(res.rows);
+    }
+    client.end()
+  })
 });
 
 app.use("/static", express.static('build/static'));
--- src/App.js
+++ src/App.js
@@ -3,6 +3,25 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      products:[{name:"toto"}]
+    }
+  }
+
+  componentDidMount(){
+    console.log("componentDidMount");
+    fetch('/api')
+      .then(result => result.json())
+      .then(result => {
+        console.log(result);
+        this.setState({
+          products: result
+        });
+      })
+  }
+
   render() {
     return (
       <div className="App">
@@ -13,6 +32,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        {this.state.products[0].name}
       </div>
     );
   }
