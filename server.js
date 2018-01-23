b67f05cb78acf3e316b1a9048bea83d72d098177
Fabien Lebas <fabien.lebas@decathlon.com>
2018-01-23 13:50:49 +0100

    First commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+const port = process.env.PORT || 4000;
+
+app.use(express.static('build'));
+
+app.listen(port, function () {
+  console.log("Server listening on port:" + port);
+});
+
+app.get("*", function (request, result) {
+  result.sendFile("./build/index.html");
+});
+
+app.get("/api/products", function (request, result) {
+  result.send("Products API");
+});
